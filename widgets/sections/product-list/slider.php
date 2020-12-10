29481892f8f83b104c9bad42833ac7a49c389212
canvilled <huyminhnguyenpham@gmail.com>
2020-12-11 00:07:58 +0700

    a part of product list

--- widgets/sections/product-list/slider.php
+++ widgets/sections/product-list/slider.php
@@ -2,4 +2,9 @@
     <h3 class="<?php _e($class_preflix,'ella') ?>-product-list-title">
         <?php _e($settings['title'],'ella') ?>
     </h3>
+    <?php if($settings['description']):?>
+    <p class="<?php _e($class_preflix,'ella') ?>-product-list-description>">
+        <?php _e($settings['description'],'ella') ?>
+    </p>
+    <?php endif;?>
 </div>
