18fa18725e07fea924ec0eba7ae61a8eca236749
Marcus <imkay2712@gmail.com>
2020-12-11 17:31:00 +0700

    conflic

--- src/js/root.js
+++ src/js/root.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import './control/carousel.js';
-import './control/instagram.js';
-=======
 import './controls/carousel.js';
+import './control/instagram.js';
 import './controls/product-list.js';
 import './controls/product-tab.js';
->>>>>>> 685f9d1877e2d943d360348e8229d4f6a4107d8d
 /* Config Light box */
 lightbox.option({
     'resizeDuration': 200,
