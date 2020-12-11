168bace4f11f701b04d5a4c8afdd5707875df9fd
canvilled <huyminhnguyenpham@gmail.com>
2020-12-12 00:17:34 +0700

    full code product tab

--- src/js/controls/product-tab.js
+++ src/js/controls/product-tab.js
@@ -1,38 +1,120 @@
 jQuery( document ).ready(function($) {
     let elementorEditorCheck = document.querySelector('body.elementor-editor-active');
-    function sliderInit () {
+    let $prev = '<button type="button" class="slick-prev slick-arrow" style="display: block;"><i class="fa fa-angle-left"></i></button>';
+    let $next = '<button type="button" class="slick-next slick-arrow" style="display: block;"><i class="fa fa-angle-right"></i></button>';
+
+    const tabQueryAjax = tab =>{
+        console.log(tab)
+        let tabListGET = document.querySelector('.wa-ella-product-tab .wa-ella-product-tab-contain-tabs-list');
+        let tabContent = document.querySelector('.wa-ella-product-tab .wa-ella-product-tab-contain-slider');
+        if(tabListGET){
+            let tabsGET = Array.prototype.slice.call(tabListGET.children);
+            for(let i = 0; i < tabsGET.length; i++) {
+                tabsGET[i].classList.remove('active');
+            }
+        }
+        tab.target.classList.add('active');
+        tabContent.innerHTML = "";
+        tabContent.innerHTML += `<p class="loading" style="margin:20px 0px;text-align:center;font-size:13px; font-weight:400">Loading ... please wait ...</p>`;
+        let dataAjax = tab.target.getAttribute('data-product');
+        if(dataAjax) {
+            setTimeout(() =>{
+                jQuery.ajax({
+                    type: "post",
+                    dataType: "html",
+                    async: false,
+                    url: apiObject.rootapiurl,
+                    data: {
+                        action: "ella_api_tab",
+                        products: dataAjax
+                    },
+                    success: function (response) {
+                        let loading = document.querySelector('.loading');
+                        loading.remove();
+                        tabContent.innerHTML = `<div class="wa-ella-product-tab-contain-list">${response}</div>`;
+                        let $sliderAfter =  $('.wa-ella-product-tab .wa-ella-product-tab-contain-list');
+                        let $showPCAfter = $sliderAfter.attr("data-show-pc") || 4;
+                        let $showTabletAfter = $sliderAfter.attr("data-show-tablet") || 3;
+                        if(!$sliderAfter.hasClass('.slick-initialized')){
+                            $sliderAfter.not('.slick-initialized').slick({
+                                slidesToShow: parseInt($showPCAfter),
+                                slidesToScroll: 1,
+                                infinite: true,
+                                speed: 300,
+                                nextArrow: $next,
+                                prevArrow: $prev,
+                                responsive: [
+                                    {
+                                    breakpoint: 1024,
+                                        settings: {
+                                            slidesToShow: parseInt($showTabletAfter),
+                                            slidesToScroll: 1,
+                                        }
+                                    },
+                                    {
+                                    breakpoint: 480,
+                                        settings: {
+                                            slidesToShow: 2,
+                                            slidesToScroll: 2
+                                        }
+                                    }
+                                ],
+                            })
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log(
+                            "The following error occured: " + textStatus,
+                            errorThrown
+                        );
+                    },
+                });
+            },500)
+        }
+    }
+
+    const tabForEach = tab =>{ tab.onclick = tabQueryAjax;}
+
+    const tabsClick = () =>{
+        let tabList = document.querySelector('.wa-ella-product-tab .wa-ella-product-tab-contain-tabs-list');
+        if(tabList){
+            var tabs = Array.prototype.slice.call(tabList.children);
+            if(tabs.length !== 0){ tabs.forEach(tabForEach);}
+        }
+    }
+
+    const sliderInit = () => {
         let slider = document.querySelector('.wa-ella-product-tab .wa-ella-product-tab-contain-list');
-        var $prev = '<button type="button" class="slick-prev slick-arrow" style="display: block;"><i class="fa fa-angle-left"></i></button>';
-        var $next = '<button type="button" class="slick-next slick-arrow" style="display: block;"><i class="fa fa-angle-right"></i></button>';
         if(slider){
             var $slider =  $('.wa-ella-product-tab .wa-ella-product-tab-contain-list');
             var $showPC = $slider.attr("data-show-pc") || 4;
             var $showTablet = $slider.attr("data-show-tablet") || 3;
             if(!$slider.hasClass('.slick-initialized')){
                 $slider.not('.slick-initialized').slick({
-                slidesToShow: parseInt($showPC),
-                slidesToScroll: 1,
-                infinite: true,
-                speed: 300,
-                nextArrow: $next,
-                prevArrow: $prev,
-                responsive: [
-                    {
-                    breakpoint: 1024,
-                        settings: {
-                            slidesToShow: parseInt($showTablet),
-                            slidesToScroll: 1,
+                    slidesToShow: parseInt($showPC),
+                    slidesToScroll: 1,
+                    infinite: true,
+                    speed: 300,
+                    nextArrow: $next,
+                    prevArrow: $prev,
+                    responsive: [
+                        {
+                        breakpoint: 1024,
+                            settings: {
+                                slidesToShow: parseInt($showTablet),
+                                slidesToScroll: 1,
+                            }
+                        },
+                        {
+                        breakpoint: 480,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2
+                            }
                         }
-                    },
-                    {
-                    breakpoint: 480,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
-                        }
-                    }
-                ],
-            })
+                    ],
+                })
+                tabsClick();
             }
         }
     }
--- widgets/sections/product-tab.php
+++ widgets/sections/product-tab.php
@@ -1,9 +1,11 @@
 <div class="<?php _e($class_preflix,'ella') ?>-product-tab">
+    <?php if($settings['title']):?>
     <h3 class="<?php _e($class_preflix,'ella') ?>-product-tab-title">
         <span>
             <?php _e($settings['title'],'ella') ?>
         </span>
     </h3>
+    <?php endif;?>
     <?php if($settings['description']):?>
     <p class="<?php _e($class_preflix,'ella') ?>-product-tab-description">
         <?php _e($settings['description'],'ella') ?>
@@ -12,31 +14,35 @@
     <?php if($settings['list']):?>
         <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain">
             <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-tabs">
-            <?php foreach($settings['list'] as $item): ?>
-                <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-tabs-item" data-product="<?php _e(implode(", ",$item['show_elements']),'ella')  ?>">
-                    <?php 
-                        _e($item['list_title'],'ella');
-                    ?>
-                </div>
-            <?php endforeach;?>
-                <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-list" data-show-pc="<?php _e($settings['slider_item_pc'],'ella'); ?>" data-show-tablet="<?php _e($settings['slider_item_tablet'],'ella'); ?>">
-                <?php foreach($settings['list'][0]['show_elements'] as $post): ?>
-                    <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-list-item">
-                    <?php  
-                        $params = array(
-                            'p' => (int)$post, 
-                            'post_type' => 'product'    
-                        );
-                        $wc_query = new WP_Query($params);
-                        if ($wc_query->have_posts()) :
-                            while ($wc_query->have_posts()) : $wc_query->the_post(); 
-                                    get_template_part('sections/product-item-slider');
-                            endwhile;
-                            wp_reset_postdata();
-                        endif;
-                    ?>
+                <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-tabs-list <?php if($settings['tab_style'] == 'style2'){echo 'special--style';} ?>">
+                <?php $count=0; foreach($settings['list'] as $item): ?>
+                    <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-tabs-item <?php if($count === 0){echo 'active';} ?>" data-product="<?php _e(implode(", ",$item['show_elements']),'ella')  ?>">
+                        <?php 
+                            _e($item['list_title'],'ella');
+                        ?>
                     </div>
-                <?php endforeach;?>
+                <?php $count++; endforeach;?>
+                </div>
+                <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-slider">
+                    <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-list" data-show-pc="<?php _e($settings['slider_item_pc'],'ella'); ?>" data-show-tablet="<?php _e($settings['slider_item_tablet'],'ella'); ?>">
+                    <?php foreach($settings['list'][0]['show_elements'] as $post): ?>
+                        <div class="<?php _e($class_preflix,'ella') ?>-product-tab-contain-list-item">
+                        <?php  
+                            $params = array(
+                                'p' => (int)$post, 
+                                'post_type' => 'product'    
+                            );
+                            $wc_query = new WP_Query($params);
+                            if ($wc_query->have_posts()) :
+                                while ($wc_query->have_posts()) : $wc_query->the_post(); 
+                                        get_template_part('sections/product-item-slider');
+                                endwhile;
+                                wp_reset_postdata();
+                            endif;
+                        ?>
+                        </div>
+                    <?php endforeach;?>
+                    </div>
                 </div>
             </div>
         <div>
