72b8b64c4ecb7c696749f930d51feba5c6df1de5
canvilled <huyminhnguyenpham@gmail.com>
2020-12-14 05:45:14 +0700

    full product tab with banner

--- src/js/controls/product-tab-with-banner.js
+++ src/js/controls/product-tab-with-banner.js
@@ -1,5 +1,100 @@
 jQuery( document ).ready(function($) {
     let elementorEditorCheck = document.querySelector('body.elementor-editor-active');
+    let $prev = '<button type="button" class="slick-prev slick-arrow" style="display: block;"><i class="fa fa-angle-left"></i></button>';
+    let $next = '<button type="button" class="slick-next slick-arrow" style="display: block;"><i class="fa fa-angle-right"></i></button>';
+    
+    function tabQueryAjax (tab){
+        let tabListGET = tab.target.parentElement;
+        let tabTop = tabListGET.parentElement;
+        let tabListContain = tabTop.nextElementSibling;
+        let tabContent = tabListContain.children[1].children[0].children[0];
+        let tabParent = tabTop.parentElement;
+        let tabsCurrent = document.querySelectorAll('.wa-ella-product-tab-with-banner');
+        var count;
+        if(tabsCurrent.length !== 0){
+            for(let j = 0; j < tabsCurrent.length; j++){
+                if(tabsCurrent[j] == tabParent){
+                    count = j
+                }
+            }
+        }
+        if(tabListGET){
+            let tabsGET = Array.prototype.slice.call(tabListGET.children);
+            for(let i = 0; i < tabsGET.length; i++) {
+                tabsGET[i].classList.remove('active');
+            }
+        }
+        tab.target.classList.add('active');
+        tabContent.innerHTML = "";
+        tabContent.innerHTML += `<p class="loading" style="margin:20px 0px;text-align:center;font-size:13px; font-weight:400">Loading ... please wait ...</p>`;
+        let dataAjax = tab.target.getAttribute('data-product');
+        if(dataAjax) {
+            setTimeout(() =>{
+                jQuery.ajax({
+                    type: "post",
+                    dataType: "html",
+                    async: false,
+                    url: apiObject.rootapiurl,
+                    data: {
+                        action: "ella_api_tab",
+                        products: dataAjax
+                    },
+                    success: function (response) {
+                        let loading = document.querySelector('.loading');
+                        loading.remove();
+                        tabContent.innerHTML = `<div class="wa-ella-product-tab-with-banner-container-product-contain-products">${response}</div>`;
+                        let $sliderAfter =  $('.wa-ella-product-tab-with-banner .wa-ella-product-tab-with-banner-container-product-contain-products').eq(count);
+                        console.log($sliderAfter);
+                        let $showPCAfter = tabContent.getAttribute("data-show-pc") || 4;
+                        let $showTabletAfter = tabContent.getAttribute("data-show-tablet") || 3;
+                        $sliderAfter.not('.slick-initialized').slick({
+                            slidesToShow: parseInt($showPCAfter),
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: true,
+                            speed: 300,
+                            nextArrow: $next,
+                            prevArrow: $prev,
+                            responsive: [
+                                {
+                                breakpoint: 1024,
+                                    settings: {
+                                        slidesToShow: parseInt($showTabletAfter),
+                                        slidesToScroll: 1,
+                                    }
+                                },
+                                {
+                                breakpoint: 480,
+                                    settings: {
+                                        slidesToShow: 2,
+                                        slidesToScroll: 2,
+                                    }
+                                }
+                            ],
+                        })
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log(
+                            "The following error occured: " + textStatus,
+                            errorThrown
+                        );
+                    },
+                });
+            },500)
+        }
+    }
+    
+    function tabInitProductSlider(){
+        let tabLists = document.querySelectorAll('.wa-ella-product-tab-with-banner .wa-ella-product-tab-with-banner-top-tab');
+        if(tabLists.length !== 0){
+            tabLists.forEach((tabList,index)=>{
+                var tabs = Array.prototype.slice.call(tabList.children);
+                if(tabs.length !== 0){ tabs.forEach((tab)=>{
+                    tab.onclick = tabQueryAjax;
+                });}
+            })
+        }
+    }
 
     function sliderBannerInit(){
         let sliderBanner = document.querySelector('.wa-ella-product-tab-with-banner .wa-ella-product-tab-with-banner-container-image-list');
@@ -60,6 +155,7 @@ jQuery( document ).ready(function($) {
                     ],
                 })
             });
+            tabInitProductSlider();
         }
     }
 
