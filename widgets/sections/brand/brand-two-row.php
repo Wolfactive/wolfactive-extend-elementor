3b1457e41984d46749c6149affcc9b2d9ff0c410
canvilled <huyminhnguyenpham@gmail.com>
2020-12-27 16:31:23 +0700

    add toggle autoplay insta slider

--- widgets/sections/brand/brand-two-row.php
+++ widgets/sections/brand/brand-two-row.php
@@ -6,11 +6,9 @@
     <div class="<?php _e($class_preflix,'ella') ?>-description-brand">
         <?php _e($settings['brand_description'],'ella') ?>
     </div>
+    <div class="<?php _e($class_preflix,'ella') ?>-slider-brand-mobile">
         <?php foreach (  $settings['list'] as $index=>$item ): 
-            if($index == 0): ?>
-            <div class="<?php _e($class_preflix,'ella') ?>-slider-brand-mobile">
-            <?php endif;    
-                if($index % 2 != 0):
+                if($index % 2 == 0):
             ?>
             <div class="<?php _e($class_preflix,'ella') ?>-slider-brand-mobile-item col-divide-5-col col-divide-sm-12">
                 <a href="<?php echo  $item['link_brand']['url']; ?>" class="<?php _e($class_preflix,'ella') ?>-image-brand">
@@ -19,22 +17,14 @@
                     ?>
                 </a>
             <?php endif; ?>
-            <?php if($index % 2 == 0):?>
+            <?php if($index % 2 != 0):?>
                 <a href="<?php echo  $item['link_brand']['url']; ?>" class="<?php _e($class_preflix,'ella') ?>-image-brand">
                     <?php
                         echo '<img src="' . $item['list_image']['url'] . '">';
                     ?>
                 </a>
-            </div>
-            <?php endif;?>
-            <?php if(($index + 1) == count($settings['list'])):?>
-                <a href="<?php echo  $item['link_brand']['url']; ?>" class="<?php _e($class_preflix,'ella') ?>-image-brand">
-                    <?php
-                        echo '<img src="' . $item['list_image']['url'] . '">';
-                    ?>
-                </a>
-            </div>
             </div>
             <?php endif;?>
         <?php endforeach; ?>
+    </div>
 </div>
