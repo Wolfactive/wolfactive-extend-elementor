f62a6dd9c0293328bdb1c28f44b739e07ac8d7f9
canvilled <huyminhnguyenpham@gmail.com>
2021-01-04 00:32:50 +0700

    fix

--- src/js/components/loadMoreButton.js
+++ src/js/components/loadMoreButton.js
@@ -1,25 +1,26 @@
 const buttonForEach = (item) =>{
     let button = item.querySelector('button[aria-label="load-more-button-product-list"]');
     let products = Array.prototype.slice.call(item.querySelectorAll('.wa-ella-product-list-contain-item'));
-    
     let row = parseInt(item.getAttribute('data-plus'));
     let show = parseInt(item.getAttribute('data-item')),i;
-    button.onclick = () => {
-        if(show < products.length){
-            for( i = 0 ; i < products.length; i++){
-                if(i >= show){
-                    products[i].classList.remove('d--none');
-                } 
-                if(i == products.length - 1){
-                    button.remove();
-                } 
-                if(i == (show + row - 1)){
-                    item.setAttribute('data-item',(show + row));
-                    break;
-                } 
+    try {
+        button.onclick = () => {
+            if(show < products.length){
+                for( i = 0 ; i < products.length; i++){
+                    if(i >= show){
+                        products[i].classList.remove('d--none');
+                    } 
+                    if(i == products.length - 1){
+                        button.remove();
+                    } 
+                    if(i == (show + row - 1)){
+                        item.setAttribute('data-item',(show + row));
+                        break;
+                    } 
+                }
             }
         }
-    }
+    } catch (e) {}
 }
 
 const loadMoreButton = () =>{
